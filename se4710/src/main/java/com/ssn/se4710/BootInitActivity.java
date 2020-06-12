77eb1a894c6475ae76b9dc3a02ab659dec5dec5a
leihly1252 <leihly1252@gmail.com>
2020-06-12 15:50:43 +0800

    v1.9.7，优化初始化流程

--- se4710/src/main/java/com/ssn/se4710/BootInitActivity.java
+++ se4710/src/main/java/com/ssn/se4710/BootInitActivity.java
@@ -2,12 +2,23 @@ package com.ssn.se4710;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.hhw.ssn.comutils.LogUtils;
+import com.ssn.se4710.dao.Symbology;
+import com.ssn.se4710.dao.SymbologyDao;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author HuangLei 1252065297@qq.com
@@ -20,8 +31,16 @@ import com.hhw.ssn.comutils.LogUtils;
  */
 public class BootInitActivity extends Activity {
 
+    private ThreadFactory threadFactory = Executors.defaultThreadFactory();
+    private ExecutorService mExecutorService = new ThreadPoolExecutor(1, 200, 0L,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(1024), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+
     private final String TAG = BootInitActivity.class.getSimpleName();
 
+    /**
+     * 上下文环境，用来获取配置信息
+     */
+    private Context mContext;
 
 //    private static class MessageHandler extends Handler {
 //        private WeakReference<BootInitActivity> mWeakReference;
@@ -51,23 +70,19 @@ public class BootInitActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boot);
-//        mHandler = new MessageHandler(this);
-        boolean isBoot = this.getIntent().getBooleanExtra("isBoot", true);
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        BootInitFragment bootInitFragment = BootInitFragment.newInstance(getApplicationContext(), isBoot);
-        transaction.add(R.id.content, bootInitFragment);
-        transaction.commit();
+        // 获取应用上下文
+        mContext = this.getApplicationContext();
     }
 
     public void startService() {
         LogUtils.e(TAG, "startService, ");
 //        if (mIsBoot) {
-        Intent toService = new Intent(this.getApplicationContext(), Se4710Service.class);
+        Intent toService = new Intent(mContext, Se4710Service.class);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            this.getApplicationContext().startForegroundService(toService);
+            mContext.startForegroundService(toService);
             LogUtils.i(TAG, "onCreate-0");
         } else {
-            this.getApplicationContext().startService(toService);
+            mContext.startService(toService);
             LogUtils.i(TAG, "onCreate-1");
         }
 //        }
@@ -76,7 +91,55 @@ public class BootInitActivity extends Activity {
 
     @Override
     protected void onResume() {
-        super.onResume();
+        // 初始化Se4710码制配置表
+        mExecutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                SymbologyDao symbologyDao = Se4710Application.getInstances().getDaoSession().getSymbologyDao();
+                long count = symbologyDao.count();
+                LogUtils.e(TAG, "onCreate, count:" + count);
+                if (count <= 0) {
+                    TypedArray array = mContext.getResources().obtainTypedArray(R.array.symbologies_child);
+                    int[] paramNum = mContext.getResources().getIntArray(R.array.symbology_param_num);
+                    int[] paramValueDef = mContext.getResources().getIntArray(R.array.symbology_param_value_def);
+                    int index = 0;
+                    for (int i = 0; i < array.length(); i++) {
+                        String[] tempString = mContext.getResources().getStringArray(array.getResourceId(i, -1));
+                        for (String s : tempString) {
+                            Symbology symbology = new Symbology();
+                            symbology.setParamNum(paramNum[index]);
+                            symbology.setParamName(s);
+                            symbology.setParamValueDef(paramValueDef[index]);
+                            symbology.setParamValue(paramValueDef[index]);
+                            symbologyDao.insert(symbology);
+                            index++;
+                        }
+                    }
+                    array.recycle();
+//                    int[] intArray = getResources().getIntArray(R.array.symbologies_parameter);
+//                    int[] defArray = getResources().getIntArray(R.array.default_enabled);
+//                    String[] stringArray = getResources().getStringArray(R.array.symbologies);
+//                    for (int i = 0; i < intArray.length; i++) {
+//                        Symbology symbology = new Symbology();
+//                        symbology.setParamNum((long) intArray[i]);
+//                        symbology.setName(stringArray[i]);
+//                        LogUtils.e(TAG, "onCreate, symbology:" + stringArray[i]);
+//                        symbology.setEnabled(defArray[i]);
+//                        symbologyDao.insert(symbology);
+//                    }
+                }
+                // 启动扫描服务
+                startService();
+                LogUtils.e(TAG, "onCreate init database successfully: ");
+//                mHandler.sendEmptyMessage(0);
+            }
+        });
+//        mHandler = new MessageHandler(this);
+        boolean isBoot = this.getIntent().getBooleanExtra("isBoot", true);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        BootInitFragment bootInitFragment = BootInitFragment.newInstance(getApplicationContext(), isBoot);
+        transaction.add(R.id.content, bootInitFragment);
+        transaction.commit();
 //        try {
 //            Thread.sleep(30000);
 //            this.finish();
@@ -86,9 +149,10 @@ public class BootInitActivity extends Activity {
 //        }
 //        try {
 //            Thread.sleep(800);
-        BootInitActivity.this.finish();
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
+        super.onResume();
+        finish();
     }
 }
